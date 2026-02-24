c552e6337887ef28922308fbcbbaecc80da63039
Jonathan Wahrenberg <jonathan@wahrenberg.com>
2026-02-24 18:24:34 +0100

    refactor: Collapsed ADD into emit_binop (used to be emit_norm_binop). Removed redundant pointer case for ADD.

--- src/x64_codegen.c
+++ src/x64_codegen.c
@@ -222,11 +222,13 @@ static void x64_emit_div_or_mod(FILE *out, const FrameLayout *fl,
     }
 }
 
-// Handles SUB, MUL, AND, OR, XOR, LOGICAL_AND and LOGICAL_OR
-static const char *norm_binop_mnem(IrOp op) {
+// Handles ADD, SUB, MUL, AND, OR, XOR, LOGICAL_AND and LOGICAL_OR
+static const char *binop_mnem(IrOp op) {
     // LOGICAL_AND/LOGICAL_OR are the same as AND/OR since lowering guarantees
     // bool types
     switch (op) {
+    case IR_OP_ADD:
+        return "add";
     case IR_OP_SUB:
         return "sub";
     case IR_OP_MUL:
@@ -247,9 +249,9 @@ static const char *norm_binop_mnem(IrOp op) {
     }
 }
 
-// Handles SUB, MUL, AND, OR, XOR, LOGICAL_AND and LOGICAL_OR
-static void x64_emit_norm_binop(FILE *out, const FrameLayout *fl,
-                                const IrInst *inst) {
+// Handles ADD, SUB, MUL, AND, OR, XOR, LOGICAL_AND and LOGICAL_OR
+static void x64_emit_binop(FILE *out, const FrameLayout *fl,
+                           const IrInst *inst) {
     TypeId t             = inst->type;
     CgSizeInfo size_info = cg_size_info(t);
 
@@ -260,7 +262,7 @@ static void x64_emit_norm_binop(FILE *out, const FrameLayout *fl,
     fprintf(out, "    mov %s, %s [rbp%+d]\n", size_info.reg, size_info.mem,
             off_src0);
 
-    const char *mnem = norm_binop_mnem(inst->op);
+    const char *mnem = binop_mnem(inst->op);
     fprintf(out, "    %s %s, %s [rbp%+d]\n", mnem, size_info.reg, size_info.mem,
             off_src1);
 
@@ -314,32 +316,7 @@ static void x64_emit_inst(FILE *out, const IrFunc *fn, const FrameLayout *fl,
         break;
     }
 
-    case IR_OP_ADD: {
-        TypeId t             = inst->type;
-        CgSizeInfo size_info = cg_size_info(t);
-
-        int off_dst          = stack_offset_for_value(fl, inst->dst);
-        int off0             = stack_offset_for_value(fl, inst->src0);
-        int off1             = stack_offset_for_value(fl, inst->src1);
-
-        if (t == TYPEID_PTR) {
-            // Pointer arithmetic
-            fprintf(out, "    mov rax, QWORD PTR [rbp%+d]\n", off0);
-            fprintf(out, "    mov rcx, QWORD PTR [rbp%+d]\n", off1);
-            fprintf(out, "    add rax, rcx\n");
-            fprintf(out, "    mov QWORD PTR [rbp%+d], rax\n", off_dst);
-        } else {
-            // Normal integer add
-            fprintf(out, "    mov %s, %s [rbp%+d]\n", size_info.reg,
-                    size_info.mem, off0);
-            fprintf(out, "    add %s, %s [rbp%+d]\n", size_info.reg,
-                    size_info.mem, off1);
-            fprintf(out, "    mov %s [rbp%+d], %s\n", size_info.mem, off_dst,
-                    size_info.reg);
-        }
-        break;
-    }
-
+    case IR_OP_ADD:
     case IR_OP_SUB:
     case IR_OP_MUL:
     case IR_OP_AND:
@@ -347,7 +324,7 @@ static void x64_emit_inst(FILE *out, const IrFunc *fn, const FrameLayout *fl,
     case IR_OP_XOR:
     case IR_OP_LOGICAL_AND:
     case IR_OP_LOGICAL_OR: {
-        x64_emit_norm_binop(out, fl, inst);
+        x64_emit_binop(out, fl, inst);
         break;
     }
 
